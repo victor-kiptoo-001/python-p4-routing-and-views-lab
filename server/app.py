801c45efc1f190a35559ef339021c6476673334d
victor-kiptoo-001 <victor.kiptoo1@student.moringaschool.com>
2023-09-19 17:24:53 +0300

    Initial commit

--- server/app.py
+++ server/app.py
@@ -4,5 +4,35 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:str_param>')
+def print_string(str_param):
+    print('hello')
+    return f"hello"
+
+@app.route('/count/<int:parameter>')
+def count(parameter):
+    return '\n'.join(map(str, range(parameter))) + '\n'
+
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        result = num1 / num2
+    elif operation == '%':
+        result = num1 % num2
+    else:
+        result = "Invalid operation"
+
+    return str(result)
+
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(debug=True)
